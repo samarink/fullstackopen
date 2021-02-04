290e5f6547c04a744b0b3a4ecc4bba49cf6caf4b
Konstantin Samarin <konstsamari@gmail.com>
2021-02-04 16:42:02 +0700

    exercise 1.4

--- part1/new-app/src/index.js
+++ part1/new-app/src/index.js
@@ -3,39 +3,35 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
-  const total = part1.exercises + part2.exercises + part3.exercises;
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
+  const total = parts.reduce((a, b) => a + b, 0);
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
       <Total number={total} />
     </>
   );
 };
 
-const Header = (props) => {
-  console.log(props);
-  return <h1>{props.course}</h1>;
-};
-
+const Header = (props) => <h1>{props.course}</h1>;
 const Content = (props) => (
   <>
-    <Part name={props.part1.name} exercises={props.part1.exercises} />
-    <Part name={props.part2.name} exercises={props.part2.exercises} />
-    <Part name={props.part3.name} exercises={props.part3.exercises} />
+    {props.parts.map(part => <Part name={part.name} exercises={part.exercises} />)}
   </>
 );
 
