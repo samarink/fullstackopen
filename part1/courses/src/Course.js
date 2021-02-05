635a6f36994e9351ca4669edc1438175a043db3c
Konstantin Samarin <konstsamari@gmail.com>
2021-02-05 15:28:25 +0700

    exercise 2.4

--- part1/courses/src/Course.js
+++ part1/courses/src/Course.js
@@ -1,13 +1,17 @@
 import Header from './Header';
 import Content from './Content';
 
-const Course = ({ course: { name, parts } }) => {
-  const total = parts.reduce((a, b) => a + b.exercises, 0);
+const Course = ({ courses }) => {
+  const calcTotal = (parts) => parts.reduce((a, b) => a + b.exercises, 0);
 
   return (
     <>
-      <Header text={name} />
-      <Content parts={parts} total={total} />
+      {courses.map(({ name, parts, id }) => (
+        <div key={id}>
+          <Header text={name} />
+          <Content parts={parts} total={calcTotal(parts)} />
+        </div>
+      ))}
     </>
   );
 };
