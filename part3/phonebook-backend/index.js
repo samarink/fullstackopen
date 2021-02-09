7f574239986f6313fe936e99d01b21cab2d28ac3
Konstantin Samarin <konstsamari@gmail.com>
2021-02-09 19:38:11 +0700

    exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const persons = [
+let persons = [
   {
     name: 'Arto Hellas',
     number: '040-123456',
@@ -45,6 +45,13 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
 });
 
 const PORT = process.env.PORT || 3001;
