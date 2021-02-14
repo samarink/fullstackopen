0a34be046d462362fa82da93190ba14ef4668ab1
Konstantin Samarin <konstsamari@gmail.com>
2021-02-14 18:44:03 +0700

    exercise 4.5

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -6,7 +6,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((a, b) => a + b.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const reducer = (max, cur) => (max.likes > cur.likes ? max : cur);
+
+  return blogs.reduce(reducer, {});
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
