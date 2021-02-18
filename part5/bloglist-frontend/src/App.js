21be688b779bbab2b6ee1cc45274ffd04d7c078e
Konstantin Samarin <konstsamari@gmail.com>
2021-02-19 00:01:13 +0700

    exercise 5.4 + refactor

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Blog from './components/Blog';
+import Blogs from './components/Blogs';
+import LoginForm from './components/LoginForm';
+import BlogForm from './components/BlogForm';
+import Notification from './components/Notification';
 import blogService from './services/blogs';
 import loginService from './services/login';
 
@@ -11,6 +14,7 @@ const App = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
   const [url, setUrl] = useState('');
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -41,6 +45,8 @@ const App = () => {
       setUsername('');
       setPassword('');
     } catch (exception) {
+      setMessage('Username or password incorrect');
+      setTimeout(() => setMessage(null), 3000);
       console.log(exception);
     }
   };
@@ -61,86 +67,40 @@ const App = () => {
     setAuthor('');
     setUrl('');
     setBlogs([...blogs, newBlog]);
+    setMessage(`A new blog ${newBlog.title} by ${newBlog.author} added`);
+    setTimeout(() => setMessage(null), 3000);
   };
-
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
-
-  const blogsContainer = () => (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
-    </div>
-  );
-
-  const newBlogForm = () => (
-    <form onSubmit={createNewBlog}>
-      <div>
-        title
-        <input
-          type="text"
-          value={title}
-          name="title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author
-        <input
-          type="text"
-          value={author}
-          name="author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        url
-        <input
-          type="text"
-          value={url}
-          name="url"
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">add blog</button>
-    </form>
-  );
 
   return (
     <>
       {user ? (
         <>
+          <Notification message={message} />
           <p>{user.username} is logged in</p>
           <button onClick={logout}>log out</button>
           <h2>Create new</h2>
-          {newBlogForm()}
-          {blogsContainer()}
+          <BlogForm
+            createNewBlog={createNewBlog}
+            title={title}
+            setTitle={setTitle}
+            author={author}
+            setAuthor={setAuthor}
+            url={url}
+            setUrl={setUrl}
+          />
+          <Blogs blogs={blogs} />
         </>
       ) : (
-        <>{loginForm()}</>
+        <>
+          <Notification message={message} />
+          <LoginForm
+            handleLogin={handleLogin}
+            username={username}
+            password={password}
+            setUsername={setUsername}
+            setPassword={setPassword}
+          />
+        </>
       )}
     </>
   );
