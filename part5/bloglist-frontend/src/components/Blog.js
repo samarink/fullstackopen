5c4058379536e9c7ac5b68c683116ad566f633c3
Konstantin Samarin <konstsamari@gmail.com>
2021-02-20 01:02:31 +0700

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,41 @@
-import React from 'react';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from 'react';
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+const Blog = ({ blog: { title, author, url, likes } }) => {
+  const [fullView, setFullView] = useState(false);
+
+  const toggleFullView = () => setFullView(!fullView);
+
+  return (
+    <div style={blogStyle}>
+      {fullView ? (
+        <div>
+          <p>
+            {title} {author}
+          </p>
+          <a href={url}>read</a>
+          <p>
+            {likes}
+            <button>like</button>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <p>
+            {title} {author}
+          </p>
+        </div>
+      )}
+      <button onClick={toggleFullView}>{fullView ? 'hide' : 'show'}</button>
+    </div>
+  );
+};
 
 export default Blog;
