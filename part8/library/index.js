85804627ade339496b9554b2891550465de6a07c
Konstantin Samarin <konstsamari@gmail.com>
2021-03-06 00:16:22 +0700

    exercise 8.4

--- part8/library/index.js
+++ part8/library/index.js
@@ -104,7 +104,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -113,7 +113,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_, args) => {
+      return args.author
+        ? books.filter((book) => book.author === args.author)
+        : books;
+    },
     allAuthors: () => authors,
   },
   Author: {
