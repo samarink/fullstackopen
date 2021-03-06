47222c0a1ea097bb0982d6ff98591c3e48ec7766
Konstantin Samarin <konstsamari@gmail.com>
2021-03-06 23:32:38 +0700

    exercise 8.12

--- part8/library-frontend/src/components/EditAuthor.js
+++ part8/library-frontend/src/components/EditAuthor.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
-import { EDIT_AUTHOR } from '../queries';
-import { useMutation } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries';
+import { useMutation, useQuery } from '@apollo/client';
+import Select from 'react-select';
 
 const EditForm = () => {
-  const [name, setName] = useState([]);
+  const result = useQuery(ALL_AUTHORS);
   const [born, setBorn] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [authors, setAuthors] = useState([]);
   const [editAuthor] = useMutation(EDIT_AUTHOR);
+  const options = authors.map((author) => ({
+    value: author.name,
+    label: author.name,
+  }));
+
+  useEffect(() => {
+    if (result.data) {
+      setAuthors(result.data.allAuthors);
+    }
+  }, [result]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    editAuthor({ variables: { name, setBornTo: born } });
+    editAuthor({ variables: { name: selectedOption.value, setBornTo: born } });
 
-    setName('');
     setBorn('');
   };
 
@@ -20,13 +32,11 @@ const EditForm = () => {
     <div>
       <h2>Set Birthyear</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
         <div>
           born
           <input
