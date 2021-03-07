aab6541bd5c0aeb90ed83541076d2bdf6b7be4b1
Konstantin Samarin <konstsamari@gmail.com>
2021-03-07 23:59:58 +0700

    exercise 8.14

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,4 +1,3 @@
-const { v1: uuid } = require('uuid');
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
 const Author = require('./models/author');
@@ -17,91 +16,6 @@ mongoose
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message);
   });
-
-// {{{
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-];
-
-/*
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- */
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-];
-// }}}
 
 const typeDefs = gql`
   type Book {
@@ -140,14 +54,26 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    //    allBooks: () => Author.find({}),
+    bookCount: () => Book.find({}).count(),
+    authorCount: () => Author.find({}).count(),
+    allBooks: async (_, args) => {
+      let books = await Book.find({});
+      books = args.genre
+        ? books.filter((book) => book.genres.includes(args.genre))
+        : books;
+
+      return books;
+    },
     allAuthors: () => Author.find({}),
   },
   Author: {
     bookCount: (root) => {
-      return books.filter((book) => book.author === root.name).length;
+      return Book.find({ author: root._id }).count();
+    },
+  },
+  Book: {
+    author: (root) => {
+      return Author.findById(root.author);
     },
   },
   Mutation: {
@@ -168,18 +94,14 @@ const resolvers = {
 
       return newBook.save();
     },
-    editAuthor: (_, args) => {
-      const authorToEdit = authors.find((author) => author.name === args.name);
+    editAuthor: async (_, args) => {
+      const author = await Author.findOne({ name: args.name });
 
-      if (!authorToEdit) return null;
+      if (!author) return null;
 
-      const updatedAuthor = { ...authorToEdit, born: args.setBornTo };
+      author.born = Number(args.setBornTo);
 
-      authors = authors.map((author) =>
-        author.id === updatedAuthor.id ? updatedAuthor : author
-      );
-
-      return updatedAuthor;
+      return author.save();
     },
   },
 };
