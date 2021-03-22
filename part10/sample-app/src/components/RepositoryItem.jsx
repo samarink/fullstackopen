ccf63711912002ba893b21ff7f39e0e217d2dfea
Konstantin Samarin <konstsamari@gmail.com>
2021-03-22 19:35:48 +0700

    exercise 10.3

---
+++ part10/sample-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RepositoryItem = ({
+  fullName,
+  description,
+  language,
+  forksCount,
+  reviewCount,
+  ratingAverage,
+}) => {
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
