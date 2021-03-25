c5817ff5bae10a38683e4fb2b8984108021e575d
Konstantin Samarin <konstsamari@gmail.com>
2021-03-25 18:40:21 +0700

    enhance apollo client request, use react context for dependency injection

--- part10/rate-repository-app/src/hooks/useSignIn.js
+++ part10/rate-repository-app/src/hooks/useSignIn.js
@@ -1,7 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { AUTHORIZE } from '../graphql/mutations';
 
+import useAuthStorage from '../hooks/useAuthStorage';
+
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(AUTHORIZE);
 
   const signIn = async ({ username, password }) =>
