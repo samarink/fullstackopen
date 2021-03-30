736e724ea38fd3f2de3257b47ede2bc6802b8b16
Konstantin Samarin <konstsamari@gmail.com>
2021-03-30 18:48:28 +0700

    exercise 10.24

--- part10/rate-repository-app/src/components/RepositoryList.jsx
+++ part10/rate-repository-app/src/components/RepositoryList.jsx
@@ -1,23 +1,58 @@
 import React, { useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
-import useRepositories from '../hooks/useRepositories';
 import { FlatList } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { useDebounce } from 'use-debounce';
+import useRepositories from '../hooks/useRepositories';
 import RepositoryItem from './RepositoryItem';
 import ItemSeparator from './ItemSeparator';
 
 const RepositoryList = () => {
   const { repositories, refetch } = useRepositories();
   const [variables, setVariables] = useState();
-
-  const onValueChange = (itemValue) => {
-    setVariables(itemValue);
-    const variables = JSON.parse(itemValue);
-    refetch(variables);
-  };
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchKeyword] = useDebounce(searchQuery, 500);
 
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
+
+  const onValueChange = (itemValue) => {
+    setVariables(itemValue);
+
+    const variables = JSON.parse(itemValue);
+    refetch(variables);
+  };
+
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+
+    refetch({ searchKeyword });
+  };
+
+  const renderHeader = () => (
+    <>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <Picker selectedValue={variables} onValueChange={onValueChange}>
+        <Picker.Item
+          label="Latest repositories"
+          value='{ "orderBy": "CREATED_AT" }'
+        />
+        <Picker.Item
+          label="Highest rated repositories"
+          value='{ "orderBy": "RATING_AVERAGE" }'
+        />
+        <Picker.Item
+          label="Lowest rated repositories"
+          value='{ "orderBy": "RATING_AVERAGE", "orderDirection": "ASC" }'
+        />
+      </Picker>
+    </>
+  );
 
   return (
     <FlatList
@@ -25,22 +60,7 @@ const RepositoryList = () => {
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem {...item} />}
       keyExtractor={(item) => item.id}
-      ListHeaderComponent={
-        <Picker selectedValue={variables} onValueChange={onValueChange}>
-          <Picker.Item
-            label="Latest repositories"
-            value='{ "orderBy": "CREATED_AT" }'
-          />
-          <Picker.Item
-            label="Highest rated repositories"
-            value='{ "orderBy": "RATING_AVERAGE" }'
-          />
-          <Picker.Item
-            label="Lowest rated repositories"
-            value='{ "orderBy": "RATING_AVERAGE", "orderDirection": "ASC" }'
-          />
-        </Picker>
-      }
+      ListHeaderComponent={renderHeader()}
     />
   );
 };
